749721d22a1d566d624adf2ccb5cbdd31a0e4816
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2022-01-17 14:52:12 +0100

    departental fixes

--- admin/enroll.php
+++ admin/enroll.php
@@ -157,9 +157,9 @@ include("functions/top.php");
                                     <label>Department.:</label>
                                     <select name="dept" id="dept" class="custom-select">
                                         <option name="dept" id="dept">Null</option>
-                                        <option name="dept" id="dept">Nursery School</option>
-                                        <option name="dept" id="dept">Primary School</option>
-                                        <option name="dept" id="dept">Secondary School</option>
+                                        <option name="dept" id="dept">Art</option>
+                                        <option name="dept" id="dept">Science</option>
+                                        <option name="dept" id="dept">Commercial</option>
 
                                     </select>
                                 </div>
